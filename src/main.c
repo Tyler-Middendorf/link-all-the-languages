b4adc8393984c724baf127ef46052ab0ebd55902
Tyler Middendorf <34323666+Tyler-Middendorf@users.noreply.github.com>
2019-11-17 16:42:47 -0600

    Add java?

--- src/main.c
+++ src/main.c
@@ -1,6 +1,6 @@
 #include <functions.h>
 
-const void (*hello[])(void) = {hello_rust};
+const void (*hello[])(void) = {hello_rust,helloJava};
 
 #define NUMBER_OF_LANGUAGES sizeof(hello) / sizeof(hello[0])
 
